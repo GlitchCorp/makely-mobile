8180014026a8bd210fbb367a6ec806ac8bc2ec47
Mariusz Masztalerczuk <mariusz@masztalerczuk.com>
2021-11-04 21:34:45 +0100

    Add fb and google button

--- lib/Screens/Welcome/welcome_screen.dart
+++ lib/Screens/Welcome/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -20,9 +21,78 @@ class _LoginScreenState extends State<LoginScreen> {
                 )
             ),
 
-            body: Center(child: Text('You have pressed the button times.')),
-        )
-    );
+          // body: Stack(
+          //     children: <Widget>[
+          //       Container(
+          //         alignment: Alignment(0.7, -0.6),
+          //         child: Image.asset('assets/logos/example_logo.png')
+          //       ),
+          //       Container(
+          //           alignment: Alignment(0, 0),
+          //           child: Text("Rozpocznij")
+          //       ),
+          //       Container(
+          //           alignment: Alignment(0, 0.55),
+          //           height: 50.0,
+          //           width: 700.0,
+          //           child: SignInButton(
+          //             Buttons.Facebook,
+          //             onPressed: () {},
+          //           )
+          //
+          //       )
+          //     ],
+          // ),
+          body:               Container(
+            height: double.infinity,
+            child: SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              padding: EdgeInsets.symmetric(
+                horizontal: 40.0,
+                vertical: 120.0,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                        Container(
+                          alignment: Alignment(0.7, -0.6),
+                          child: Image.asset('assets/logos/example_logo.png')
+                        ),
+                        SizedBox(height: 25.0),
+                        Container(
+                            alignment: Alignment(0, 0),
+                            child: Text("Rozpocznij")
+                        ),
+                        SizedBox(height: 250.0),
+                        Container(
+                            alignment: Alignment(0, 0.115),
+                            height: 50.0,
+                            width: 700.0,
+
+                            child: SignInButton(
+                              Buttons.Facebook,
+                              onPressed: () {},
+                            )
+
+                        ),
+                  SizedBox(height: 30.0),
+                  Container(
+                      alignment: Alignment(0, 0.115),
+                      height: 50.0,
+                      width: 400.0,
+
+                      child: SignInButton(
+                        Buttons.Google,
+                        onPressed: () {},
+                      )
+
+                  )
+                ],
+              ),
+            ),
+          )
+          ),
+        );
   }
 }
 
